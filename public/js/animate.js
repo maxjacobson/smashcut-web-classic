316cc1d9c4b8a9d1af2c5c09bc0b719897cf92e2
Max Jacobson <maxwell.jacobson@gmail.com>
2012-11-18 19:10:52 -0500

    fixed animation

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,29 +1,48 @@
 $(document).ready(function(){
-  
-  var pagewidth = $('#html').width();
-  pagewidth -= 300;
-  pagewidth /= 2;
-  
+  var animateInit = $(window).width();
+  animateInit -= 300;
+  animateInit /= 2;
+  var oldWidth = $(window).width(),
+    newWidth, diff;
     $('#logo').animate({
       opacity: 1,
-      left: '+=' + pagewidth + 'px'
+      left: '+=' + animateInit + 'px'
     }, 500, function() {});
-    // $('#smash').click(function() {
-    //     $('#logo').animate({
-    //         opacity: 0,
-    //         left: '+=50px'
-    //     }, 500, function() {});
-    //     $('#pastebox').animate({
-    //         opacity: 0
-    //     }, 500, function() {});
-    //     $('#previewbutton').animate({
-    //         opacity: 0
-    //     }, 500, function() {});
-    //     $('#footer').animate({
-    //         opacity: 0
-    //     }, 500, function() {});
-    // });
+    $(window).resize(function() {
+      newWidth = $(window).width();
+      if(newWidth < oldWidth) {
+        diff = oldWidth - newWidth;
+        diff /= 2;
+        $('#logo').animate({
+          opacity: 1,
+          left: '-=' + diff + 'px'
+        }, 50, function() {});
+      } else {
+        diff = newWidth - oldWidth;
+        diff /= 2;
+        $('#logo').animate({
+          opacity: 1,
+          left: '+=' + diff + 'px'
+        }, 50, function() {});
+      }
+      oldWidth = newWidth;
+    });
 });
 
 // http://stackoverflow.com/questions/686995/jquery-catch-paste-input
 // must they press the button or can they just paste it in and have it go?
+// $('#smash').click(function() {
+//     $('#logo').animate({
+//         opacity: 0,
+//         left: '+=50px'
+//     }, 500, function() {});
+//     $('#pastebox').animate({
+//         opacity: 0
+//     }, 500, function() {});
+//     $('#previewbutton').animate({
+//         opacity: 0
+//     }, 500, function() {});
+//     $('#footer').animate({
+//         opacity: 0
+//     }, 500, function() {});
+// });

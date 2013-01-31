cf41b9a13fdf64edac160bb463b6b74d5aa35334
Max Jacobson <maxwell.jacobson@gmail.com>
2013-01-31 18:56:10 -0500

    maybe fixed that one bug, where sometimes the js wouldnt load

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // welcome to hell, kid
 
-require(['jquery', 'alertify.min', 'autosize', 'animate_logo', 'garlic.min'], function ($, alertify) {
+require(['jquery', 'autosize', 'animate_logo', 'garlic.min'], function ($, autosize) {
   $(document).ready(function () {
 
     // uses a plugin to autosize the textarea as it gets populated
@@ -8,18 +8,24 @@ require(['jquery', 'alertify.min', 'autosize', 'animate_logo', 'garlic.min'], fu
 
     if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
       // should I detect other stuff?
-      $("#load_button").css("display", "none");
+    } else {
+      $("#load_button").css("display", "inline");
     }
-
-    if ($(document).width() <= 480) {
+    if ($(document).width() <= 300) {
+      $("button").addClass("btn-mini");
+    } else if ($(document).width() <= 480) {
       $("button").addClass("btn-small");
     }
     $(window).resize(function() {
       var current_width = $(window).width();
       if (current_width <= 480) {
         $("button").addClass("btn-small");
+        if (current_width <= 300) {
+          $("button").addClass("btn-mini");
+        }
       } else {
         $("button").removeClass("btn-small");
+        $("button").removeClass("btn-mini");
       }
     });
 
@@ -32,15 +38,12 @@ require(['jquery', 'alertify.min', 'autosize', 'animate_logo', 'garlic.min'], fu
         };
         var current_text = $("#fountain").val();
         if (current_text === "" || current_text == demo) {
-          alertify.log( "Please dont judge my writing.", "info" );
           insert_demo();
         } else {
           var conf = confirm("This will replace the current text.");
           if (conf === true) {
             insert_demo();
-            alertify.log("Please don't judge my writing.", "info");
           } else {
-            alertify.log("Demo canceled!", "canceled");
           }
         }
       });
@@ -49,43 +52,28 @@ require(['jquery', 'alertify.min', 'autosize', 'animate_logo', 'garlic.min'], fu
       $("#clear").click(function() {
         var current_text = $("#fountain").val();
         if (current_text === "") {
-          alertify.error("Already cleared!");
           $("#specify_filename").val("");
-          $("#fountain").css("height", "350px");
         } else if (current_text == demo) { // eh, you can clear that w/o confirming
           $("#fountain").val("").trigger('autosize');
           $("#specify_filename").val("");
-          // $("#fountain").css("height", "350px");
-          alertify.log("Now get writing!", "nag");
-          alertify.success("Cleared away demo!!");
         } else {
           var conf = confirm("You sure?");
           if (conf === true) {
             $("#fountain").val("").trigger('autosize');
             $("#specify_filename").val("");
-            // $("#fountain").css("height", "350px");
-            alertify.success("Cleared!");
           } else {
-            alertify.log("Clear canceled!", "canceled");
           }
         }
       });
 
-
-
       // will submit the form
       $("#smash").click(function () {
         var current_text = $("#fountain").val();
-        if (current_text === "") {
-          alertify.error("It's blank!");
-        } else {
-          alertify.error("Processing doesn't work yet");
-          // alertify.log("Processing...", "thinking");
-          // $("#screenplay_form").submit();
-          // fix this: you can hit enter in the filename box to avoid this logic
-          // you can submit blank pages if you just avoid clicking that button
-          // so bind to the whole submit event somehow http://api.jquery.com/submit/
-        }
+        alert("disabled for now");
+        // $("#screenplay_form").submit();
+        // fix this: you can hit enter in the filename box to avoid this logic
+        // you can submit blank pages if you just avoid clicking that button
+        // so bind to the whole submit event somehow http://api.jquery.com/submit/
       });
 
       // the actual file input is hidden so this button is
@@ -102,7 +90,6 @@ require(['jquery', 'alertify.min', 'autosize', 'animate_logo', 'garlic.min'], fu
         reader.onload = function(event) {
           $("#fountain").val(event.target.result).trigger('autosize');
           $("#specify_filename").val(name.replace(/\..+$/, "")); // removes file extension from filename and puts in the little box... should it?
-          alertify.log ("Loaded " + name);
         };
         if (name.match(pattern)) {
           var current_text = $("#fountain").val();
@@ -112,16 +99,12 @@ require(['jquery', 'alertify.min', 'autosize', 'animate_logo', 'garlic.min'], fu
             var conf = confirm("This will replace the current text.");
             if (conf === true) {
               reader.readAsText(selected_file);
-            } else {
-              alertify.log("Canceled!", "canceled");
             }
           }
         } else {
           alert("Bad file extension. Please use .fountain");
         }
       });
-
-
     });
   });
 });
